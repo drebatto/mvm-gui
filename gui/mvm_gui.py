59c9ca5fc4c29e3616da1f851e82a5a2b162842e
David Rebatto <david.rebatto@mi.infn.it>
2020-04-25 23:20:32 +0200

    Refactor code to make pylint happy.

--- gui/mvm_gui.py
+++ gui/mvm_gui.py
@@ -6,9 +6,7 @@ Runs the MVM GUI
 import sys
 import os
 import os.path
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QMessageBox
-from PyQt5 import uic
+from PyQt5 import QtCore, QtWidgets
 
 import yaml
 
@@ -16,44 +14,45 @@ from mainwindow import MainWindow
 from communication.esp32serial import ESP32Serial
 from communication.fake_esp32serial import FakeESP32Serial
 from messagebox import MessageBox
+from serial.serialutil import SerialException
 
-def connect_esp32(config):
-    try:
-        if 'fakeESP32' in sys.argv:
-            print('******* Simulating communication with ESP32')
-            err_msg = "Cannot setup FakeESP32Serial"
-            esp32 = FakeESP32Serial(config)
-            esp32.set("wdenable", 1)
-        else:
-            err_msg = "Cannot communicate with port %s" % config['port']
-            esp32 = ESP32Serial(config)
-            esp32.set("wdenable", 1)
-    except Exception as error:
-        msg = MessageBox()
-        fn = msg.critical("Do you want to retry?",
-                          "Severe hardware communication error",
-                          str(error) + err_msg, "Communication error",
-                          { msg.Retry: lambda: connect_esp32(config),
-                            msg.Abort: lambda: None})
-        return fn()
-
-    return esp32
-
-
-if __name__ == "__main__":
+def start_gui():
+    """
+    Launch the MVM GUI
+    """
     base_dir = os.path.dirname(__file__)
     settings_file = os.path.join(base_dir, 'default_settings.yaml')
 
-    with open(settings_file) as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
+    with open(settings_file) as mvm_settings:
+        config = yaml.load(mvm_settings, Loader=yaml.FullLoader)
     print('Config:', yaml.dump(config), sep='\n')
 
     app = QtWidgets.QApplication(sys.argv)
 
-    esp32 = connect_esp32(config)
+    esp32 = None
+    if 'fakeESP32' in sys.argv:
+        print('******* Simulating communication with ESP32')
+        esp32 = FakeESP32Serial(config)
+    else:
+        while True:
+            try:
+                err_msg = "Cannot communicate with port %s" % config['port']
+                esp32 = ESP32Serial(config)
+                break
+            except SerialException as error:
+                msg = MessageBox()
+                retry = msg.critical("Do you want to retry?",
+                                     "Severe hardware communication error",
+                                     str(error) + err_msg, "Communication error",
+                                     {msg.Retry: lambda: True,
+                                      msg.Abort: lambda: sys.exit(-1)})
+                if not retry():
+                    break
 
     if esp32 is None:
         exit(-1)
+
+    esp32.set("wdenable", 1)
 
     watchdog = QtCore.QTimer()
     watchdog.timeout.connect(esp32.set_watchdog)
@@ -64,3 +63,5 @@ if __name__ == "__main__":
     app.exec_()
     esp32.set("wdenable", 0)
 
+if __name__ == "__main__":
+    start_gui()
